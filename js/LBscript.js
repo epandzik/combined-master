b36e30597f0720104d9b1f46e9d026ce99760820
epandzik <epandzik@gmail.com>
2015-08-05 14:41:16 -0500

    just going to fancy box

---
+++ js/LBscript.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+	$("#LBimages a").lightBox();
+});
+
--- js/easyLB.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-  $("a#show-panel").click(function(){
-    $("#lightbox, #lightbox-panel").fadeIn(300);
-  });
-    $("a#close-panel").click(function(){
-    $("#lightbox, #lightbox-panel").fadeOut(300);
-  })
-});
